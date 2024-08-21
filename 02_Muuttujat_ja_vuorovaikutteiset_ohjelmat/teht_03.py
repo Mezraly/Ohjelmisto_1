e0fd4d1b255f9af7e39e786fb2f3f2a7f0481825
Mezraly <aatuhir@metropolia.fi>
2024-08-21 09:01:00 +0300

    tehtävä

--- 02_Muuttujat_ja_vuorovaikutteiset_ohjelmat/teht_03.py
+++ 02_Muuttujat_ja_vuorovaikutteiset_ohjelmat/teht_03.py
@@ -1,1 +1,8 @@
-testi
+x = int(input("Suorakulmion kanta: "))
+h = int(input("Suorakulmion korkeus: "))
+
+piiri = 2*x + 2*h
+ala = x*h
+
+print(f"Suorakulmion piiri: {piiri}")
+print(f"Suorakulmion pinta-ala: {ala}")
