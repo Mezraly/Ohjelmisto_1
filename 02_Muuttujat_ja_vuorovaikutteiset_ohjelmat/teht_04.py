82ae2def8f4fa1f17dcdc4e97c16dd05060013d6
Mezraly <aatuhir@metropolia.fi>
2024-08-21 09:26:08 +0300

    tehtävä 4

---
+++ 02_Muuttujat_ja_vuorovaikutteiset_ohjelmat/teht_04.py
@@ -0,0 +1,11 @@
+l1 = int(input("Luku 1: "))
+l2 = int(input("Luku 2: "))
+l3 = int(input("Luku 3: "))
+
+summa = l1+l2+l3
+tulo = l1*l2*l3
+keski = (l1+l2+l3)/3
+
+print(f"Lukujen summa: {summa}")
+print(f"Lukujen tulo: {tulo}")
+print(f"Lukujen keskiarvo: {keski}")
