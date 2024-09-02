2f434c49b7a8a7101203e735294b4f46c3d2b1dc
Mezraly <aatuhir@metropolia.fi>
2024-09-02 10:53:53 +0300

    07

---
+++ 07_Monikko,_joukko_ja_sanakirja/teht_01.py
@@ -0,0 +1,11 @@
+aika = ("Talvi", "Kevät", "Kesä", "Syksy")
+num = int(input("Anna kuukausi (1-12): "))
+
+if num in range(3, 6):
+    print(aika[1])
+elif num in range(6, 9):
+    print(aika[2])
+elif num in range(9, 12):
+    print(aika[3])
+if num in range(1, 3) or num == 12:
+    print(aika[0])
